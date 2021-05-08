3597d23390d3868dc25731001d28b8722686bfdd
Orhan Jan <orhancan85@gmail.com>
2021-05-08 22:36:51 +0400

    Initial commit

---
+++ src/main/java/az/ada/service/task_management/TaskManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package az.ada.service.task_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/ada/service/task_management/TaskManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ada.service.task_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
