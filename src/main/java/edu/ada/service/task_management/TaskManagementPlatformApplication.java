7321570ff626356d3ada689030305696da2c812c
Orhan Jan <orhancan85@gmail.com>
2021-05-11 18:45:51 +0400

    Minor bug fix

--- src/main/java/edu/ada/service/task_management/TaskManagementPlatformApplication.java
+++ src/main/java/edu/ada/service/task_management/TaskManagementPlatformApplication.java
@@ -3,6 +3,8 @@ package edu.ada.service.task_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//https://www.getpostman.com/collections/f168a6a7d7bc7eca0a9d
+
 @SpringBootApplication
 public class TaskManagementPlatformApplication {
 
