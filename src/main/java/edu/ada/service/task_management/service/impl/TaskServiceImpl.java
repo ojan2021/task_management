7ec407b493e1ca8d03b2fc615569d80d2ca2906e
Orhan Jan <orhancan85@gmail.com>
2021-05-11 14:27:35 +0400

    Minor update

--- src/main/java/edu/ada/service/task_management/service/impl/TaskServiceImpl.java
+++ src/main/java/edu/ada/service/task_management/service/impl/TaskServiceImpl.java
@@ -1,8 +1,39 @@
 package edu.ada.service.task_management.service.impl;
 
+import edu.ada.service.task_management.model.entity.TaskEntity;
+import edu.ada.service.task_management.service.TaskService;
 import org.springframework.stereotype.Service;
 
 @Service(value = "TaskServiceImpl")
-public class TaskServiceImpl {
+public class TaskServiceImpl implements TaskService {
 
+    @Override
+    public TaskEntity searchByTitle(String title) {
+        return null;
+    }
+
+    @Override
+    public TaskEntity searchById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void createTask() {
+
+    }
+
+    @Override
+    public void deleteTask() {
+
+    }
+
+    @Override
+    public void editTask() {
+
+    }
+
+    @Override
+    public void assignTask() {
+
+    }
 }
